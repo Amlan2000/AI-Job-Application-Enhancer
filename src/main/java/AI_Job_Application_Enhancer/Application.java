b402ccd0a0ea0b7e9f34e8d8718bd5e278c024c2
amlan2000 <amlanmishra0911@gmail.com>
2025-04-18 20:52:22 +0530

    initial commit

---
+++ src/main/java/AI_Job_Application_Enhancer/Application.java
@@ -0,0 +1,13 @@
+package AI_Job_Application_Enhancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/AI_Job_Application_Enhancer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package AI_Job_Application_Enhancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
